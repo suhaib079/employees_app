c32f818116e33a74c9dfad8b8ce30edd316d9084
suhaib079 <suhaibabdennabi33@gmail.com>
2023-04-12 14:34:04 +0300

    add routes

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/contacts/index', function () {
+    return view('index');
+})->name('all_contacts');
+Route::get('/contacts/create', function () {
+    return view('create');
+})->name('create');
+
+
